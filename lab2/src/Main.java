3722695409f0ef64eec83f99c2b4b88bd13d738a
Ernest Leśniak <114195002+ImRlyQt@users.noreply.github.com>
2024-12-08 19:34:32 +0100

    Add files via upload

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Color color1 = new Color(255, 0, 0);
-        Color color2 = new Color(0, 255, 0, 128);
+        Color color2 = new Color(0, 0, 255, 128);
 
         System.out.println("Color 1: " + color1);
         System.out.println("Color 1 - Red: " + color1.red() + ", Green: " + color1.green() + ", Blue: " + color1.blue() + ", Alpha: " + color1.alpha());
